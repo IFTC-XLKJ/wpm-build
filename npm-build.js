669ec872f196a71b274153153123af8cb337c83b
IFTC-XLKJ <iftcceo@139.com>
2025-12-14 13:40:09 +0800

    Update npm-build.js

--- npm-build.js
+++ npm-build.js
@@ -1,7 +1,7 @@
 const minVersion = '1.0';
 async function main(args) {
     if (wpm.version < minVersion) {
-        wpm.error("wpm version 1.0 or higher is required to run this script.");
+        wpm.error("wpm version " + minVersion + " or higher is required to run this script.");
         return 1;
     }
 }
